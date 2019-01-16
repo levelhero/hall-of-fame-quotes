6bb3f3dffa5810f8ff9a7f9b5d474a41a0a8f32d
Ciara Tenorio <ciara.tenorio@starfi.sh>
2019-01-16 17:34:35 +0800

    switched from tailwind css to uikit css and also added jquery and cleaned up a bit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+  var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+      slidesPerView: 1,
+      spaceBetween: 30,
+      mousewheel: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+  });
+});
